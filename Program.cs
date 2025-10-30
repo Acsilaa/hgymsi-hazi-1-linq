2d3c90c0e4a42d512184e28d042939818ccc2cc0
Attila Gál - Galeen <112324246+Acsilaa@users.noreply.github.com>
2025-10-30 11:43:31 +0000

    .

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿List<int> napiCsapadek = new List<int> { 0, 0, 1, 0, 0, 2, 0, 0, 3, 1, 0, 0, 0, 0, 4, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 2, 0, 3, 0, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 5, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 18, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 22, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+List<double> maxHofok = new List<double> { 29.1, 28.4, 27.8, 27.2, 26.5, 25.9, 26.0, 25.3, 24.8, 25.1, 24.4, 23.7, 23.2, 22.8, 21.9, 22.3, 21.5, 20.8, 20.2, 19.7, 20.1, 19.4, 18.8, 18.1, 17.6, 17.3, 16.9, 16.1, 15.8, 15.2 };
+List<string> versenyzok = new List<string> {"Nagy-Péter", "Kovács-Anna", "Tóth-Gábor", "Szabó-Dóra", "Varga-Balázs", "Kiss-Eszter", "Molnár-Tamás", "Németh-Kata", "Horváth-László", "Balogh-Zsófia", "Papp-Máté", "Farkas-Júlia", "Lakatos-Dávid", "Juhász-Lili", "Takács-Roland", "Mészáros-Vivien", "Oláh-Kristóf", "Simon-Laura", "Rácz-Dániel", "Fekete-Noémi", "Vass-Márton", "Bodnár-Kinga", "Fehér-Patrik", "Török-Blanka", "Gulyás-Bence", "Balázs-Viktória", "Antal-Kristina", "Fodor-Gergely", "Szalai-Emese", "Kelemen-Robert", "Major-Liliána", "Bognár-Attila", "Pintér-Erika", "Hegedűs-Kristóf", "Sipos-Alexandra", "Kocsis-Levente", "Orosz-Eszter", "Lengyel-Kristóf", "Szűcs-Ágnes", "Pál-Gábor", "Vincze-Lilla", "Vörös-Dávid", "Király-Laura", "Tóth-Miklós", "Jakab-Zita", "Lukács-Márk", "Kovács-Viktor", "Varga-Rebeka", "Kiss-Zoltán", "Szabó-Nóra", "Horváth-Kristóf", "Molnár-Dorina", "Balogh-Levente", "Papp-Emma", "Nagy-Gergely", "Németh-Vivien", "Takács-Márk", "Farkas-Luca", "Juhász-Tamás", "Vass-Klaudia", "Fehér-Gábor", "Török-Anna", "Gulyás-Dóra", "Balázs-Máté", "Antal-Kitti", "Fodor-Balázs", "Szalai-Noémi", "Kelemen-Máté", "Major-Petra", "Bognár-Dániel", "Pintér-Laura", "Hegedűs-Ádám", "Sipos-Vivien", "Kocsis-Norbert", "Orosz-Kata", "Lengyel-Bence", "Szűcs-Lili", "Pál-Márk", "Vincze-Ádám", "Vörös-Rebeka", "Király-Gergő", "Tóth-Klaudia", "Jakab-Márton", "Lukács-Dóra", "Kovács-Gábor", "Varga-Lilla", "Kiss-Dániel", "Szabó-Petra", "Horváth-Balázs", "Molnár-Noémi", "Balogh-Zoltán", "Papp-Jázmin", "Nagy-László", "Németh-Klaudia", "Takács-Gergely", "Farkas-Nóra", "Juhász-Márk", "Vass-Dorina", "Fehér-Levente", "Török-Gábor", "Gulyás-Laura", "Balázs-Kristóf", "Antal-Eszter", "Fodor-Patrik", "Szalai-Emma", "Kelemen-Dániel", "Major-Zsófia", "Bognár-Kristóf", "Pintér-Dóra", "Hegedűs-Lili", "Sipos-Márton", "Kocsis-Ágnes", "Orosz-Vivien", "Lengyel-Dorina", "Szűcs-Gergő", "Pál-Kinga", "Vincze-Tamás", "Vörös-Laura", "Király-Zsombor", "Tóth-Dorina", "Jakab-Gergely", "Lukács-Kata", "Kovács-Bence", "Varga-Jázmin", "Kiss-Márton", "Szabó-Lilla", "Horváth-Dávid", "Molnár-Rebeka", "Balogh-Kristina", "Papp-Kristóf", "Nagy-Liliána", "Németh-Márk", "Takács-Dóra", "Farkas-Zoltán", "Juhász-Vivien", "Vass-Miklós", "Fehér-Dóra", "Török-Máté", "Gulyás-Zsófia", "Balázs-Levente", "Antal-Luca", "Fodor-Dániel", "Szalai-Balázs", "Kelemen-Klaudia", "Major-Gábor", "Bognár-Lili", "Pintér-Gergő", "Hegedűs-Laura", "Sipos-Zoltán", "Kocsis-Vivien", "Orosz-Dániel", "Lengyel-Eszter", "Szűcs-Tamás", "Pál-Emma", "Vincze-Márton", "Vörös-Nóra", "Király-Ádám", "Tóth-Kinga", "Jakab-Balázs", "Lukács-Eszter" };
+List<string> diakok = new List<string>() {"Balogh Zsombor","Sopronyi Zoltán","Hőgye Dominik","Kelemen Marcell","Máté Patrik","Körtvélyessi Benedek","Szabó Zoltán","Turóczi Dávid","Nagy Balázs","Csahóczi Máté Benedek","Tasnádi Attila","Tarsoly Levente Ákos","Vas Bence","Tóth Martin","Macz Enikő","Kiss Gergő","Balogh Endre","Mozsár Máté","Félegyházi Gergő","Pápai Sándor","Orosz Levente","Kovács Ákos","Bodnár Petra","Mercs Alex","Sztrik Márton","Gellén Csaba Kálmán","Barta Ernő Péter","Bányai Gábor","Kis Ábel","Kiss Gergely","Hajdú Bettina","Bereczki István","Moldován Adrián"};
+
+// 1) Hány olyan nap volt, amikor 20 fok feletti hőmérsékletet mértek? (2p)
+Console.WriteLine("===1)===");
+Console.WriteLine(maxHofok.Count(x => x > 20));
+
+// 2) Hozzon létre egy új, hőfok szerint csökkenően rendezett listát! (3p)
+Console.WriteLine("===2)===");
+List<double> maxHofokDesc = maxHofok.OrderDescending().ToList();
+maxHofokDesc.ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 3) Hány olyan diák van, akiknek két keresztneve van? (3p)
+Console.WriteLine("===3)===");
+Console.WriteLine(diakok.Count(x => x.Split(" ").Length == 3));
+
+/* 4) Írassa képernyőre foreach vezérlési szerkezet segítségével azoknak a diákoknak a neveit, 
+akik nevének hossza meghaladja a 15 karaktert. A kiíratás a nevek hossza szerint növekedve történjen! (5p) */
+Console.WriteLine("===4)===");
+diakok.Where(x => x.Length > 15).OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+// 5) Hány olyan nap volt, amikor több mint 10mm eső esett? (2p)
+Console.WriteLine("===5)===");
+Console.WriteLine(napiCsapadek.Count(x => x > 10));
+
+// 6) Rendezze csapadékérték szerint csökkenően a listát! (2p)
+Console.WriteLine("===6)===");
+List<int> napiCsapadekDesc = napiCsapadek.OrderDescending().ToList();
+napiCsapadekDesc.ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 7) Készítsen egy új listát aligesett néven, amely a 3mm alatti csapadékértékeket tartalmazza. (3p)
+Console.WriteLine("===7)===");
+List<int> aligesett = napiCsapadek.Where(x => x < 3).ToList();
+aligesett.ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+/* 8) Rendezze a versenyzőket nevük karakterszáma szerint növekvő sorrendben,
+azon belül az azonos hosszúságú neveket pedig abc sorrend szerint növekedve! (4p) */
+Console.WriteLine("===8)===");
+versenyzok.Select(x => x.Replace("-", " ")).OrderBy(x => x.Length).ThenBy(x => x).ToList().ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 9) Hány olyan nap volt, amikor nem eset eső? (2p)
+Console.WriteLine("===9)===");
+Console.WriteLine(napiCsapadek.Count(x => x == 0));
+
+// 10) Készítsen egy új listát sokeso néven, amely az 5mm feletti csapadékértékeket tartalmazza. (3p)
+Console.WriteLine("===10)===");
+List<int> sokeso = napiCsapadek.Where(x => x>5).ToList();
+sokeso.ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 11) Listázza ki azoknak a versenyzőknek a nevét, akik keresztneve [Dávid] (4p)
+Console.WriteLine("===11)===");
+versenyzok.Where(x => x.Split("-")[1] == "Dávid").Select(x => x.Replace("-", " ")).ToList().ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 12)  Mekkora volt a legnagyobb hőmérsékletkülönbség a mérések között? (2p)
+Console.WriteLine("===12)===");
+Console.WriteLine(maxHofok.Max() - maxHofok.Min());
+
+// 13) Mi volt az év során a második legmagasabb csapadékérték? (3p)
+Console.WriteLine("===13)===");
+Console.WriteLine(napiCsapadek.OrderDescending().ToList()[1]);
+
+// 14) Mennyi volt az év során az átlagos csapadékmennyiség? (2p)
+Console.WriteLine("===14)===");
+Console.WriteLine(napiCsapadek.Average());
+
+// 15) Van-e a versenyzők között „Szuper Béla” nevű induló? (2p)
+Console.WriteLine("===15)===");
+Console.WriteLine(versenyzok.Any(x => x == "Szuper-Béla")
+    ? "Van." : "Nincs.");
+
+// 16) Az év melyik napján (sorszám) esett utoljára 30mm feletti csapadék? (3p)
+Console.WriteLine("===16)===");
+Console.WriteLine(
+    napiCsapadek.Where(x => x > 30).Index().LastOrDefault()
+);
+
+// 17) Hány különböző vezetéknevű diák van a listában? (4p)
+Console.WriteLine("===17)===");
+Console.WriteLine(diakok.DistinctBy(x => x.Split(" ")[0]).Count());
+
+// 18) Hogyan hívják a legelső olyan versenyzőt, akinek a nevében szerepel a „Vajk” szó? (3p)
+Console.WriteLine("===18)===");
+Console.WriteLine(versenyzok.Where(x => x.Contains("Vajk")).Select(x => x.Replace("-", " ")).FirstOrDefault());
+
+// 19) Készítsen egy nevek listát, amelyben a diákoknak csak az első keresztnevük szerepel! (3p)
+Console.WriteLine("===19)===");
+List<string> nevek = diakok.Select(x => x.Split(" ")[1]).ToList();
+nevek.ForEach(x => Console.Write(x + ", "));
+Console.WriteLine();
+
+// 20) Készítsen egy angolul nevű listát fordított névrend szerint a Béla nevű versenyzőkről. (4p)
+Console.WriteLine("===20)===");
+List<string> angolul = versenyzok.Where(x => x.Contains("-Béla"))
+    .Select(x => x + " " + x.Split("-")[0])
+    .Select(x => x.Replace("-", " "))
+    .Select(x => x.Replace(x.Split(" ")[0] + " ", "")).ToList();
+
+// ez a megoldás megtartja a több keresztnév sorrendjét is, így helyesen "fordítva" a neveket.
+// pl.: Gál Attila Péter => Attila Péter Gál
+
+angolul.ForEach(x => Console.WriteLine(x));
